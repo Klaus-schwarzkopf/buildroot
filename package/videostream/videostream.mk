1c26fc4e31dcbea40d0e45de3d1ec5bf99434e34
Klaus Schwarzkopf <schwarzkopf@sensortherm.de>
2012-01-20 15:32:49 +0100

    fixed i18n support for external toolchain

--- package/videostream/videostream.mk
+++ package/videostream/videostream.mk
@@ -25,16 +25,25 @@ define VIDEOSTREAM_INSTALL_TARGET_CMDS
 	cp -a $(@D)/icons/tango/22x22/categories/preferences-system.png $(TARGET_DIR)/usr/share/icons/videostream/
 	cp -a $(@D)/icons/tango/16x16/actions/go-last.png $(TARGET_DIR)/usr/share/icons/videostream/
 #copy program
-	cp -a $(@D)/Debug/videostream $(TARGET_DIR)/usr/bin/
+	-cp -a $(@D)/Debug/videostream $(TARGET_DIR)/usr/bin/
 
 #copy load modules
 	cp -a $(@D)/files/loadmodules_dm365.sh $(TARGET_DIR)/usr/bin/
 
 #copy translations
-	mkdir -p $(TARGET_DIR)/usr/share/locale/{de,en,ru}/LC_MESSAGES/
-	- cp -R $(@D)/po/de/videostream.mo $(TARGET_DIR)/usr/share/locale/de/LC_MESSAGES/
-	- cp -R $(@D)/po/en/videostream.mo $(TARGET_DIR)/usr/share/locale/en/LC_MESSAGES/
-	- cp -R $(@D)/po/ru/videostream.mo $(TARGET_DIR)/usr/share/locale/ru/LC_MESSAGES/
+	mkdir -p $(TARGET_DIR)/usr/lib/locale/
+
+	    for lang in en_GB.utf8 fr_FR.utf8 ru_RU.utf8 de_DE.utf8 es_ES.utf8; do \
+		cp -R $(HOST_DIR)/opt/ext-toolchain/arm-none-linux-gnueabi/libc/usr/lib/locale/$$lang ${TARGET_DIR}/usr/lib/locale/ ; \
+	    done
+
+	    - for lang in en fr ru de es; do \
+		mkdir -p $(TARGET_DIR)/usr/share/locale/$$lang/LC_MESSAGES/ ; \
+		cp -R $(@D)/po/$$lang/videostream.mo $(TARGET_DIR)/usr/share/locale/$$lang/LC_MESSAGES/ ; \
+	    done
+
+
+
 #ifeq ($(BR2_PACKAGE_VIDEOSTREAM_DEBUG),y)
 #copy files for testing (delete for release)
 #	cp $(@D)/files/raw.yuv $(TARGET_DIR)
@@ -48,5 +57,17 @@ define VIDEOSTREAM_INSTALL_TARGET_CMDS
 endef
 
 
+i18n:
+	mkdir -p $(TARGET_DIR)/usr/share/locale/{de,en,ru,fr,es}/LC_MESSAGES/
+	- cp -R $(@D)/po/de/videostream.mo $(TARGET_DIR)/usr/share/locale/de/LC_MESSAGES/
+	- cp -R $(@D)/po/en/videostream.mo $(TARGET_DIR)/usr/share/locale/en/LC_MESSAGES/
+	- cp -R $(@D)/po/ru/videostream.mo $(TARGET_DIR)/usr/share/locale/ru/LC_MESSAGES/
+	- cp -R $(@D)/po/fr/videostream.mo $(TARGET_DIR)/usr/share/locale/fr/LC_MESSAGES/
+	- cp -R $(@D)/po/fr/videostream.mo $(TARGET_DIR)/usr/share/locale/es/LC_MESSAGES/
+	cp -R $(HOST_DIR)/opt/ext-toolchain/arm-none-linux-gnueabi/libc/usr/lib/locale/en_GB.utf8 ${TARGET_DIR}/usr/lib/locale/
+	cp -R $(HOST_DIR)/opt/ext-toolchain/arm-none-linux-gnueabi/libc/usr/lib/locale/fr_FR.utf8 ${TARGET_DIR}/usr/lib/locale/
+	cp -R $(HOST_DIR)/opt/ext-toolchain/arm-none-linux-gnueabi/libc/usr/lib/locale/ru_RU.utf8 ${TARGET_DIR}/usr/lib/locale/
+	cp -R $(HOST_DIR)/opt/ext-toolchain/arm-none-linux-gnueabi/libc/usr/lib/locale/de_DE.utf8 ${TARGET_DIR}/usr/lib/locale/
+	cp -R $(HOST_DIR)/opt/ext-toolchain/arm-none-linux-gnueabi/libc/usr/lib/locale/es_ES.utf8 ${TARGET_DIR}/usr/lib/locale/
 
 $(eval $(call GENTARGETS))
