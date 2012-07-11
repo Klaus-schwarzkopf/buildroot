863e846d061594a1d48ee0300bb60b44fc8b9f19
Klaus Schwarzkopf <schwarzkopf@sensortherm.de>
2012-07-11 16:56:01 +0200

    clean up videostream.mk

--- package/videostream/videostream.mk
+++ package/videostream/videostream.mk
@@ -5,12 +5,10 @@
 #############################################################
 
 VIDEOSTREAM_VERSION = 1.00
-#VIDEOSTREAM_SOURCE = videostream
 VIDEOSTREAM_SITE = /home/klaus/development/handpyrometer/videostream
 VIDEOSTREAM_SITE_METHOD = local
 VIDEOSTREAM_DEPENDENCIES = directfb linux
 VIDEOSTREAM_AUTORECONF=YES
-
 
 VIDEOSTREAM_CONF_ENV = CFLAGS="${CFLAGS} -I$(@D)/../linux-custom/include"
 
@@ -19,17 +17,13 @@ define VIDEOSTREAM_INSTALL_TARGET_CMDS
 #copy font
 	mkdir -p $(TARGET_DIR)/usr/lib/fonts
 	cp -a $(@D)/ttf/DejaVuSans-Bold.ttf $(TARGET_DIR)/usr/lib/fonts/Default.ttf
+
 #copy pictures
 	mkdir -p $(TARGET_DIR)/usr/share/icons/videostream/
 	cp -a $(@D)/icons/own/*.png $(TARGET_DIR)/usr/share/icons/videostream/
 	cp -a $(@D)/icons/tango/22x22/devices/{camera-photo,camera-video,video-display}.png $(TARGET_DIR)/usr/share/icons/videostream/
 	cp -a $(@D)/icons/tango/22x22/categories/preferences-system.png $(TARGET_DIR)/usr/share/icons/videostream/
 	cp -a $(@D)/icons/tango/16x16/actions/go-last.png $(TARGET_DIR)/usr/share/icons/videostream/
-#copy program
-	-cp -a $(@D)/Debug/videostream $(TARGET_DIR)/usr/bin/
-
-#copy load modules
-	cp -a $(@D)/files/loadmodules_dm365.sh $(TARGET_DIR)/usr/bin/
 
 #copy needed files
 	cp -a $(@D)/files/S30videostream $(TARGET_DIR)/etc/init.d/
@@ -50,22 +44,6 @@ define VIDEOSTREAM_INSTALL_TARGET_CMDS
 		cp -R $(@D)/po/$$lang/videostream.mo $(TARGET_DIR)/usr/share/locale/$$lang/LC_MESSAGES/ ; \
 	done
 
-
-
-#ifeq ($(BR2_PACKAGE_VIDEOSTREAM_DEBUG),y)
-#copy files for testing (delete for release)
-#	cp $(@D)/files/raw.yuv $(TARGET_DIR)
-#	cp $(@D)/files/data.img $(TARGET_DIR)
-#else
-#copy start script
-#	cp -R $(@D)/files/S60videostream $(TARGET_DIR)/etc/init.d/
-#endif
-
-#mono /home/klaus/LeopardBoardDM365sdkEVAL2011Q2/bootloader/u-boot-2010.12-rc2-psp03.01.01.39/ti-flash-utils/src/DM36x/GNU/bc_DM36x.exe -uboot -pageSize 2048 -blockNum 25 -startAddr 0x82000000 -loadAddr 0x82000000 $(TARGET_DIR)/../images/u-boot.bin -o $(TARGET_DIR)/../images/u-boot.nand.bin
 endef
 
-
-
 $(eval $(call AUTOTARGETS))
-
-#$(eval $(call GENTARGETS))
