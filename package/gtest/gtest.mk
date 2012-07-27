56e9583333fafca49d600ccb4cfe86ac7055a80a
Klaus Schwarzkopf <schwarzkopf@sensortherm.de>
2012-07-27 09:31:51 +0200

    gtest: install to staging

--- package/gtest/gtest.mk
+++ package/gtest/gtest.mk
@@ -7,9 +7,15 @@
 GTEST_VERSION = 1.6.0
 GTEST_SOURCE = gtest-$(GTEST_VERSION).zip
 GTEST_SITE = http://googletest.googlecode.com/files
+GTEST_INSTALL_STAGING = YES
 
 define GTEST_INSTALL_TARGET_CMDS
 	cp -a $(@D)/*.a $(TARGET_DIR)/usr/lib/
+endef
+
+define GTEST_INSTALL_STAGING_CMDS
+	cp -a $(@D)/*.a $(STAGING_DIR)/usr/lib/
+	cp -ar $(@D)/include/gtest $(STAGING_DIR)/usr/include/
 endef
 
 $(eval $(call CMAKETARGETS))
